ad823f4c9ccc6cfccf9578deea1d55d3b6db8063
matismed <32781801+matismed@users.noreply.github.com>
2026-03-12 09:01:51 -0300

    Update quiniela_er.js

--- api/quiniela_er.js
+++ api/quiniela_er.js
@@ -1,4 +1,4 @@
-// SCRAPER TURISTA - tujugada.com.ar DEFINITIVO
+// SCRAPER TURISTA - tujugada.com.ar 
 export default async function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Content-Type', 'application/json; charset=utf-8');
